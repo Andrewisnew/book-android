b3ee0f8db2fa25c336948025e18692994d3ba3c5
Andrey <zakomornii1998@gmail.com>
2019-03-22 22:18:04 +0300

    added twoactivities module (activity lifecycle)

---
+++ twoactivities/src/main/java/com/book/andrewisnew/twoactivities/Main2Activity.java
@@ -0,0 +1,54 @@
+package com.book.andrewisnew.twoactivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private static final String TAG = "TAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Log.d(TAG, "ActivityTwo: onCreate()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "ActivityTwo: onRestart()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "ActivityTwo: onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "ActivityTwo: onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "ActivityTwo: onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "ActivityTwo: onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "ActivityTwo: onDestroy()");
+    }
+
+}
